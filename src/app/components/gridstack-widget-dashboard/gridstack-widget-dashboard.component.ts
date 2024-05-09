a5c59b1bfad740ce1bf028fb7c52dc19277b0b7c
randika-se <randika@inbooks.se>
2024-05-09 13:54:01 +0530

    main implementation done using gridstack library.

--- src/app/components/gridstack-widget-dashboard/gridstack-widget-dashboard.component.ts
+++ src/app/components/gridstack-widget-dashboard/gridstack-widget-dashboard.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { GraniteDragDropGridComponent } from './granite-drag-drop-grid/granite-drag-drop-grid.component';
-import { WidgetLayoutActionsPanelComponent } from '../katoid-widget-layout/widget-layout-actions-panel/widget-layout-actions-panel.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { GridStackOptions } from 'gridstack';
 import { GridStackWidgetItem } from '../../models/widget.models';
-import { generateRandomUUID } from '../../utils/randomInfo';
-import { SampleWidget1Component } from '../gridster-grid-layout/sample-widgets/sample-widget-1/sample-widget-1.component';
 import { generateRandomLightColor } from '../../utils/colors';
+import { generateRandomUUID } from '../../utils/randomInfo';
+import { AddWidgetDialogComponent } from '../gridster-grid-layout/add-widget-dialog/add-widget-dialog.component';
+import { SampleWidget1Component } from '../gridster-grid-layout/sample-widgets/sample-widget-1/sample-widget-1.component';
 import { SampleWidget2Component } from '../gridster-grid-layout/sample-widgets/sample-widget-2/sample-widget-2.component';
+import { WidgetLayoutActionsPanelComponent } from '../katoid-widget-layout/widget-layout-actions-panel/widget-layout-actions-panel.component';
+import { GraniteDragDropGridComponent } from './granite-drag-drop-grid/granite-drag-drop-grid.component';
 
 @Component({
   selector: 'gridstack-widget-dashboard',
@@ -16,6 +18,8 @@ import { SampleWidget2Component } from '../gridster-grid-layout/sample-widgets/s
   imports: [GraniteDragDropGridComponent, WidgetLayoutActionsPanelComponent],
 })
 export class GridstackWidgetDashboardComponent implements OnInit {
+  addWidgetDialog: MatDialog = inject(MatDialog);
+
   isEditModeOn: boolean = false;
 
   public gridConfigurations: GridStackOptions = {
@@ -30,48 +34,12 @@ export class GridstackWidgetDashboardComponent implements OnInit {
     column: 2,
     columnOpts: {
       breakpointForWindow: true,
-      breakpoints: [{ w: 1200, c: 1 }],
+      breakpoints: [{ w: 1280, c: 1 }],
       columnMax: 2,
     },
+    auto: false,
   };
-  public gridWidgetItems: GridStackWidgetItem[] = [
-    {
-      x: 0,
-      y: 0,
-      w: 1,
-      id: generateRandomUUID(),
-      component: SampleWidget1Component,
-      inputs: {
-        widgetId: 1,
-        description: 'Widget 1',
-        backgroundColor: generateRandomLightColor(),
-      },
-    },
-    {
-      x: 1,
-      y: 0,
-      w: 1,
-      id: generateRandomUUID(),
-      component: SampleWidget2Component,
-      inputs: {
-        widgetId: 2,
-        description: 'Widget 2',
-        backgroundColor: generateRandomLightColor(),
-      },
-    },
-    {
-      x: 0,
-      y: 1,
-      w: 2,
-      id: generateRandomUUID(),
-      component: SampleWidget1Component,
-      inputs: {
-        widgetId: 3,
-        description: 'Widget 1',
-        backgroundColor: generateRandomLightColor(),
-      },
-    },
-  ];
+  public gridWidgetItems: GridStackWidgetItem[] = [];
 
   constructor() {}
 
@@ -80,5 +48,39 @@ export class GridstackWidgetDashboardComponent implements OnInit {
   toggleEditMode() {
     this.isEditModeOn = !this.isEditModeOn;
   }
-  addWidgetToLayout() {}
+  addWidgetToLayout() {
+    const dialogRef = this.addWidgetDialog.open(AddWidgetDialogComponent, {
+      width: '25vw',
+      height: '50vh',
+      maxWidth: '100vw',
+      maxHeight: '100vh',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        const isSampleWidget1: boolean = Math.random() > 0.5;
+
+        this.gridWidgetItems = [
+          ...this.gridWidgetItems,
+          {
+            w: result?.data?.width === 'full' ? 2 : 1,
+            h: result?.data?.height ?? 1,
+            id: generateRandomUUID(),
+            component: isSampleWidget1
+              ? SampleWidget1Component
+              : SampleWidget2Component,
+            inputs: {
+              widgetId: generateRandomUUID(),
+              description: `Sample Widget ${isSampleWidget1 ? '1' : '2'}`,
+              backgroundColor: generateRandomLightColor(),
+            },
+            autoPosition: true,
+          },
+        ];
+
+        this.isEditModeOn = true;
+      }
+    });
+  }
 }
